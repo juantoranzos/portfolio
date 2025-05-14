7dceb63198d8cfa4ad6c7a896346a33aa3026753
Juan Toranzos <toranzos628@gmail.com>
2025-05-14 18:38:41 -0300

    cambie el servicio de envio de correos

--- src/components/FormContacto.tsx
+++ src/components/FormContacto.tsx
@@ -1,29 +1,38 @@
-import { useState, useRef } from 'react';
+import { useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import emailjs from '@emailjs/browser';
+
 
 export const FormContacto = () => {
   const [showModal, setShowModal] = useState(false);
   const handleCloseModal = () => setShowModal(false);
   const handleShowModal = () => setShowModal(true);
-  const form = useRef<HTMLFormElement>(null);
-
-  const sendEmail = (e: React.FormEvent) => {
+   const [formData, setFormData] = useState({ name: "", email: "", message: "" });
+  const [success, setSuccess] = useState(false);
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    emailjs
-      .sendForm('service_cjg2q64', 'template_uedy7nm', form.current as HTMLFormElement, 'NcdYBZ3aGRd97OHdm')
-      .then(
-        () => {
-          console.log('SUCCESS!');
-          handleCloseModal();
-        },
-        (error) => {
-          console.log('FAILED...', error.text);
-        },
-      );
+
+    const response = await fetch("https://formspree.io/f/mkgrkrnq", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      setSuccess(true);
+      setFormData({ name: "", email: "", message: "" });
+    } else {
+      alert("Ocurrió un error al enviar el formulario.");
+    }
   };
 
+
+ 
   return (
     <div className="modal-show" style={{ display: 'block', position: 'initial' }}>
       <Button variant="outline-light" onClick={handleShowModal}>
@@ -34,14 +43,15 @@ export const FormContacto = () => {
           <Modal.Title>Contactate conmigo</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <form ref={form} onSubmit={sendEmail} data-aos="fade">
-            <input type="text" placeholder="Ingresa tu nombre" className="form-control m-auto w-50 p-2 mb-2" name="user_name" required />
-            <input type="email" placeholder="Ingresa tu email" className="form-control m-auto w-50 p-2 mb-2" name="user_email"  required/>
-            <input type="text" placeholder="Asunto" className="form-control m-auto w-50 p-2 mb-2" name="subject" required />
-            <textarea name="message" id="mensaje" placeholder="Escribe tu mensaje aqui" className="form-control m-auto w-50 p-2 mb-2" required></textarea>
+          <form onSubmit={handleSubmit} data-aos="fade" action="https://formspree.io/f/mkgrkrnq"
+  method="POST">
+            <input type="text" placeholder="Ingresa tu nombre" className="form-control m-auto w-50 p-2 mb-2" name="name" required value={formData.name} onChange={handleChange} />
+            <input type="email" placeholder="Ingresa tu email" className="form-control m-auto w-50 p-2 mb-2" name="email"  required value={formData.email} onChange={handleChange}/>
+            <textarea name="message" id="mensaje" placeholder="Escribe tu mensaje aqui" className="form-control m-auto w-50 p-2 mb-2" required value={formData.message} onChange={handleChange}></textarea>
             <div className="text-center">
               <input type="submit" value="Send" className="btn btn-dark" />
             </div>
+            {success && <p className="mt-4 text-green-600 text-center">¡Mensaje enviado correctamente!</p>}
           </form>
         </Modal.Body>
         <Modal.Footer></Modal.Footer>
